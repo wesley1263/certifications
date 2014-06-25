e45989122a2e228d75cb7c7715b207c837881c1b
wesley1263 <webdesigner.ibc@gmail.com>
2014-06-25 14:24:12 -0300

    creating type

--- application/modules/admin/controllers/TypeController.php
+++ application/modules/admin/controllers/TypeController.php
@@ -11,12 +11,26 @@ class Admin_TypeController extends Zend_Controller_Action
        if(!$this->view->session){
           $this->_redirect('/admin'); 
        }
-
+       $this->model = new Application_Model_DbTable_Type();
+       $this->language = new Application_Model_DbTable_Language();
     }
 
     public function indexAction()
     {
-        // action body
+        $this->view->listAll = $this->model->listAll();
+        $this->view->language = $this->language->listAll();
+        $this->view->success = $this->_helper->FlashMessenger->getMessages('success');
+        $this->view->error = $this->_helper->FlashMessenger->getMessages('error');
+    }
+    
+    public function newAction()
+    {
+        
+    }
+    
+    public function removeAction()
+    {
+        
     }
 
 
